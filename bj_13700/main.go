dbe094b68a257d981fcf5440fb8e6a7ca4952583
FelixSeol <felixxseol@gmail.com>
2017-11-14 16:25:08 +0900

    bj_13700 : stop

--- bj_13700/main.go
+++ bj_13700/main.go
@@ -1,23 +1,29 @@
 package main
 
 import "fmt"
-import "errors"
+import "math"
 
 var n, s, d, f, b, k int
 var land []string
 var police []int
+var cnt []int
 
 func main() {
 	fmt.Scanf("%d%d%d%d%d%d\n", &n, &s, &d, &f, &b, &k)
 	fmt.Printf("%d %d %d %d %d %d\n", n, s, d, f, b, k)
-	tmp := make([]string, n)
-	land = append(land, tmp...)
+	land = append(land, make([]string, n)...)
+	cnt = append(cnt, make([]int, n)...)
+	for i := range cnt {
+		cnt[i] = math.MaxInt32
+	}
+	cnt[s-1] = 0
+	fmt.Println(cnt)
 	for i := 0; i < k; i++ {
 		var p int
 		fmt.Scanf("%d", &p)
 		police = append(police, p)
 	}
-	fmt.Println(police[0], police[1], police[2], police[3])
+
 	setData := func() {
 		for i := range land {
 			land[i] = "O"
@@ -41,26 +47,36 @@ func main() {
 
 	setData()
 	showInGraphic()
-	r, err := play(s - 1)
-	fmt.Println(r, err)
+	result, _ := play(d - 1)
+	fmt.Println(result)
 }
 
-func play(_s int) (int, error) {
-	var r int
-	var err error
+func play(pos int) (_cnt int, _err error) {
 
-	if land[_s-1] == "K" || land[_s-1] == "C" || _s-1 < 0 || _s > n {
-		err = errors.New("BUG REPORT")
-	} else {
-		if land[_s-1+f] == "D" {
-			return r + 1, err
-		}
-		if land[_s-1+f] == "K" {
-			r, err = play(_s - b)
+	min := func(a int, b int) int {
+		if a < b {
+			return a
 		} else {
-			r, err = play(_s + f)
+			return b
 		}
-		r += 1
 	}
-	return r, err
+	if cnt[pos] != math.MaxInt32 {
+		return cnt[pos], nil
+	}
+	var fromfront, fromback int
+	if pos-f < 0 || land[pos-f] == "K" {
+		fromback = n + 1
+	} else {
+		fromback, _ = play(pos - f)
+	}
+
+	if pos+b > n || land[pos+b] == "K" {
+		fromfront = n + 1
+
+	} else {
+		fromfront, _ = play(pos + b)
+	}
+
+	cnt[pos] = min(fromback, fromfront)
+	return _cnt + 1, nil
 }
